25460ea98cdb4eaf09103c36dce01add4b9cb7a4
Nashildamudi <162422291+Nashildamudi@users.noreply.github.com>
2025-09-19 17:26:52 +0000

    dataType

---
+++ 01_basics/dataType-summery.js
@@ -0,0 +1,29 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.5
+
+const isLoggedIn = false
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+// reference (Non primitive)
+
+// Array, Objects, Functions
+
+const fruit = ["banana", "apple", "shitafal"];
+
+let MyObj = {
+    name : "nashil",
+    age : 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
