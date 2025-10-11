b0dcedef0d1b254c47a06076800fb3e4ae0020b5
Nashildamudi <162422291+Nashildamudi@users.noreply.github.com>
2025-10-11 12:48:52 +0000

    oops

---
+++ Assignment/stud.js
@@ -0,0 +1,11 @@
+let students = [
+  { name: "Ali", age: 18, marks: 85 },
+  { name: "Zara", age: 19, marks: 92 },
+  { name: "John", age: 17, marks: 78 },
+];
+
+
+let toppers = students.filter
+ students.filter( students => students.marks > 80 )
+console.log(toppers);
+
