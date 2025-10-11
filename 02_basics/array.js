126f9d5689bd93bb3b7efb8ffb221d11712e3ba0
Nashildamudi <162422291+Nashildamudi@users.noreply.github.com>
2025-10-11 09:55:13 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,16 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+
+
+// myArr.push(6)
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(7)
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(9))
+
+
+console.log("A ", myArray);
+
