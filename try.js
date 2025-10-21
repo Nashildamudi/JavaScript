4338e9636e221ec9d97422a43310a380b4063d64
Nashildamudi <162422291+Nashildamudi@users.noreply.github.com>
2025-10-21 14:06:34 +0000

    typescript

---
+++ try.js
@@ -0,0 +1,7 @@
+function tableeee(n) {
+    for (var i = 1; i <= 10; i++) {
+        console.log(n * i);
+    }
+}
+tableeee(5);
+console.log(tableeee);
---
+++ try.ts
@@ -0,0 +1,9 @@
+function tableeee(n: number): void {
+    for(let i=1; i<=10; i++) {
+        console.log(n*i);
+        
+    }
+}
+
+tableeee(5)
+console.log(tableeee);
